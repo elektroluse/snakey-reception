7b77e601e1f39312974da42560b30cf98270a9c5
elektroluse <pinkbean@hotmail.no>
2025-10-16 20:27:28 +0200

    Bcrypt dependency and set as default hashing alg

--- src/config/settings.py
+++ src/config/settings.py
@@ -104,6 +104,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Password hashes
+# Top entry is used as default (Bcrypt)
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
