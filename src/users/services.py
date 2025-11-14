f8e7370a0fd4499a996af167894ec9b11a2b5846
elektroluse <pinkbean@hotmail.no>
2025-11-14 16:20:52 +0100

    deleted comment describes functionality handled in the view layer

--- src/users/services.py
+++ src/users/services.py
@@ -26,10 +26,6 @@ class UserService:
             return serializer.data
         else :
             raise ValidationError("User data is not valid")
-        """
-            implement some event to send a registration confirmation
-            to a future email service...
-        """
   
 
     @staticmethod
